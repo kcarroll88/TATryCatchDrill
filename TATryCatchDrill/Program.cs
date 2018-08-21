84044faccf5439105afe83d24756a7f7efbc72a5
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-21 13:05:53 -0400

    Try Catch Testing

--- TATryCatchDrill/Program.cs
+++ TATryCatchDrill/Program.cs
@@ -10,6 +10,28 @@ namespace TATryCatchDrill
     {
         static void Main(string[] args)
         {
+            List<int> numbers = new List<int>();
+            numbers.Add(23);
+            numbers.Add(14);
+            numbers.Add(36);
+            numbers.Add(47);
+            Console.WriteLine("Choose a number.");
+           
+            try
+            {
+                int inputNum = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    Console.WriteLine(numbers[i] / inputNum);
+                }
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }            
+            Console.WriteLine("We've emerged from the try catch!");
+            Console.ReadLine();                      
         }
     }
 }
